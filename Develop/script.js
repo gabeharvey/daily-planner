a4894eb4e239eafaa77b55c70473d20ec32dadc7
Gabe Harvey <harvey7@gmail.com>
2023-12-30 12:40:27 -0600

    added click function to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,12 @@ let todaysDate = dayjs();
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(function () { 
+  $(".saveBtn").on("click", function () {
+    let option = $(this).parent().attr("id");
+    let utility = $(this).siblings("textarea").val();
+    localStorage.setItem(option, utility);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -37,9 +42,8 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 });
-
+//TODO: Add code to display the current date in the header of the page.
 //Time function on top of page centered
 setInterval (function() {
   $("#currentDay").text(todaysDate.format("dddd, MM/DD/YYYY, h:mm A"));
