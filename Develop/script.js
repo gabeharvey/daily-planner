0fce61452210b27e39e45d6d225524e3be72e6e7
Gabe Harvey <harvey7@gmail.com>
2024-01-01 21:29:31 -0600

    comments left in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -24,15 +24,16 @@ $(".time-block").each(function () {
   }
 });
 
-  $("#hour-9 textarea").val(localStorage.getItem("hour-9"))
-  $("#hour-10 textarea").val(localStorage.getItem("hour-10"))
-  $("#hour-11 textarea").val(localStorage.getItem("hour-11"))
-  $("#hour-12 textarea").val(localStorage.getItem("hour-12"))
-  $("#hour-13 textarea").val(localStorage.getItem("hour-13"))
-  $("#hour-14 textarea").val(localStorage.getItem("hour-14"))
-  $("#hour-15 textarea").val(localStorage.getItem("hour-15"))
-  $("#hour-16 textarea").val(localStorage.getItem("hour-16"))
-  $("#hour-17 textarea").val(localStorage.getItem("hour-17"))
+//Following elements obtain info from Local Storage and display in text area
+    $("#hour-9 textarea").val(localStorage.getItem("hour-9"))
+    $("#hour-10 textarea").val(localStorage.getItem("hour-10"))
+    $("#hour-11 textarea").val(localStorage.getItem("hour-11"))
+    $("#hour-12 textarea").val(localStorage.getItem("hour-12"))
+    $("#hour-13 textarea").val(localStorage.getItem("hour-13"))
+    $("#hour-14 textarea").val(localStorage.getItem("hour-14"))
+    $("#hour-15 textarea").val(localStorage.getItem("hour-15"))
+    $("#hour-16 textarea").val(localStorage.getItem("hour-16"))
+    $("#hour-17 textarea").val(localStorage.getItem("hour-17"))
 });
   
 //Time function on top of page centered
